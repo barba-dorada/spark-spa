c783ee3f7a37ff5261eb15d7ca237b3c01dae033
tishenko <vad.tishenko@gmail.com>
2016-10-18 12:13:37 +0300

    add devMode

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,14 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        staticFiles.location("/public");
+        boolean devMode = true;
+        if (devMode) {
+            String projectDir = ".";//System.getProperty("user.dir");
+            String staticDir = "/src/main/resources/public";
+            staticFiles.externalLocation(projectDir + staticDir);
+        } else {
+            staticFiles.location("/public");
+        }
         get("/hello", (req, res) -> "Hello World");
         System.out.println("http://localhost:4567/hello");
         System.out.println("http://localhost:4567");
